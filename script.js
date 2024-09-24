a263d996fda44178bf033ae9266dd857a5e75604
sheikhmuhammadzain <zaingee320@gmail.com>
2024-09-24 20:41:32 +0500

    completed app

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ document.getElementById('signInBtn').addEventListener('click', function() {
     // Change the style for Sign In
     document.getElementById('signUpBtn').classList.remove('active');
     document.getElementById('signInBtn').classList.add('active');
+    alert("Thank you for signing in!");
 });
 document.querySelector('.close-btn').addEventListener('click', function() {
     document.querySelector('.overlay').style.display = 'none';
-    alert("Thank you for signing up!");
 });
  
 document.getElementById('signUpBtn').addEventListener('click', function() {
     // Change the style for Sign Up
     document.getElementById('signInBtn').classList.remove('active');
     document.getElementById('signUpBtn').classList.add('active');
+    alert("Thank you for signing up!");
 });
 
 document.getElementById('form').addEventListener('submit', function(e) {
